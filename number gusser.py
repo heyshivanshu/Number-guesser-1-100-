0b401e3108154a60de8fd761fed0e2e3eccd0a32
Shivanshu <gameryt8574@gmail.com>
2026-09-23 20:46:10 +0530

    Revise number guessing game logic and range
    Updated the guessing range from 1-100 to 1-50 and improved feedback messages for user guesses.

--- number gusser.py
+++ number gusser.py
@@ -1,26 +1,31 @@
 #no of guesses 10 , print number of guesses left each time , game over when gueses end
 import random
-a = random.randint(1,100)
+a = random.randint(1,50)
 c=10
-print("Welcome to number guesser game rules are as follows\n","1) guess a number from 1-100\n","2) you have only 10 guesses")
+print("Welcome to number guesser game rules are as follows\n","1) guess a number from 1-50\n","2) you have only 10 guesses")
 while(c>0):
     print("you have",c,"guesses left")
     #print(a)
     i=int(input("enter number\n"))
     z=abs(i-a)
+    y=(a-i)
     c=c-1
     if i==a:
-        print("congratulation you have won \n,""you took",c,"guesses")
+        print(f"Congratulation you have won \n you took {10-c} guesses")
         break
-    if z>50:
+    if z>=25:
         print("far off")
-    if z<50 and z>25:
+    elif z<25 and z>=15:
         print("close but not quiet it")
-    if z<25 and z>10:
+    elif z<15 and z>=10:
         print("close")
-    if z<10 and z>5:
+    elif z<10 and z>=5:
         print("very close")
-    if z<5:
+    elif z<5:
         print("almost there")
+    if y<0:
+        print("Go lower")
+    else:
+        print("Go higher")
 if c==0:
-    print("But game over")
+    print(f"But game over random number was {a}")
